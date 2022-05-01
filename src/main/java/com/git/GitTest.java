8e60b407f2c33d3a25ffa9d4e9bf476f2c8446a7
amstlan <925227315@qq.com>
2022-05-01 15:48:48 +0800

    hotfix commit 2

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello github");
         System.out.println("hello git grate");
         System.out.println("hello git nice");
+        System.out.println("hot-fix test");
 
     }
 }
